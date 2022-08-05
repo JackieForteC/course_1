e8eda957fdb7751a1c9ee1bc894552b2f974895c
JackieForteC <110641634+JackieForteC@users.noreply.github.com>
2022-08-05 14:20:56 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    prueba += 1
+    basic.showString("" + (prueba))
+})
+input.onButtonPressed(Button.AB, function () {
+    texto1 = texto2
+    basic.showString(texto1)
+})
+input.onButtonPressed(Button.B, function () {
+    prueba += -1
+    basic.showString("" + (prueba))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString(texto1)
+})
+let texto2 = ""
+let texto1 = ""
+let prueba = 0
+prueba = 0
+texto1 = "Hola"
+texto2 = "bye"
 basic.forever(function () {
 	
 })
